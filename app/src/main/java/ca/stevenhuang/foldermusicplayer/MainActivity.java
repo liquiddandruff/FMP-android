da07060e22663b1856d86d31a676d7dfbdea567e
Steven Huang <steven@stevenhuang.ca>
2015-03-11 18:26:17 -0700

    Alignment, refactor drawer switching code

--- app/src/main/java/ca/stevenhuang/foldermusicplayer/MainActivity.java
+++ app/src/main/java/ca/stevenhuang/foldermusicplayer/MainActivity.java
@@ -33,12 +33,12 @@ import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
 
 public class MainActivity extends ActionBarActivity {
-    public static final int     DRAWER_MUSIC_ID = 10;
-    public static final String  DRAWER_MUSIC_TAG = "TAG1";
-    public static final int     DRAWER_SETTINGS_ID = 20;
-    public static final String  DRAWER_SETTINGS_TAG = "TAG2";
-    public static final int     DRAWER_INFO_ID = 30;
-    public static final String  DRAWER_INFO_TAG = "TAG3";
+	public static final int DRAWER_MUSIC_ID = 10;
+	public static final String DRAWER_MUSIC_TAG = "TAG1";
+	public static final int DRAWER_SETTINGS_ID = 20;
+	public static final String DRAWER_SETTINGS_TAG = "TAG2";
+	public static final int DRAWER_INFO_ID = 30;
+	public static final String DRAWER_INFO_TAG = "TAG3";
 
 	private static MediaMetadataRetriever mMetadata;
 	private static IPlayer mPlayer;
@@ -47,14 +47,15 @@ public class MainActivity extends ActionBarActivity {
 	private File rootMusicDir;
 
 	public void notifyResult(boolean played) {
-		if(mPlayer != null) {
-			if(played) {
+		if (mPlayer != null) {
+			if (played) {
 				Crouton.makeText(this, "Now playing " + mPlayer.getPlayable(), Style.CONFIRM).show();
 			} else {
 				Crouton.makeText(this, "File " + mPlayer.getPlayable() + " cannot be played", Style.ALERT).show();
 			}
 		}
 	}
+
 	public void playFile(File filePath) {
 		Crouton.cancelAllCroutons();
 		Playable p = new Playable(filePath.getPath());
@@ -64,7 +65,7 @@ public class MainActivity extends ActionBarActivity {
 			public void run() {
 				boolean playing = mPlayer.play();
 				notifyResult(playing);
-				if(playing) {
+				if (playing) {
 					mMetadata.setDataSource(mPlayer.getPlayable().getPath());
 					String s1 = mMetadata.extractMetadata(MediaMetadataRetriever.METADATA_KEY_BITRATE);
 					String s2 = mMetadata.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
@@ -93,92 +94,63 @@ public class MainActivity extends ActionBarActivity {
 
 		rootMusicDir = new File(Environment.getExternalStorageDirectory(), "Music");
 
-		Bundle data = new Bundle(1);
-		data.putString(Const.KEY_ROOT, rootMusicDir.getPath());
+		switchToLibraryFragment(getSupportFragmentManager().beginTransaction());
 
-		FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
-		final LibraryFragment fragment = new LibraryFragment();
-		fragment.setOnFileSelectionListener(new LibraryFragment.OnFileSelectionListener() {
-			@Override
-			public void onFileSelection(File file) {
-				playFile(file);
-			}
-		});
-		fragment.setArguments(data);
-		fragTrans.add(R.id.fragment_container, fragment, DRAWER_MUSIC_TAG);
-		fragTrans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-		fragTrans.commit();
-
-		if(mPlayer == null) {
+		if (mPlayer == null) {
 			debug("mPlayer is null, recreating...");
 			mPlayer = new Player();
 		}
-		mMetadata  = new MediaMetadataRetriever();
+		mMetadata = new MediaMetadataRetriever();
 
 
 		leftDrawer = new Drawer()
-			.withActivity(this)
-			.withToolbar(toolbar)
-			.withDrawerWidthDp(255)
-			.withDrawerGravity(Gravity.LEFT)
-			.addDrawerItems(
-				new PrimaryDrawerItem().withIdentifier(DRAWER_MUSIC_ID).withName("Home"),
-				new DividerDrawerItem(),
-				new SecondaryDrawerItem().withIdentifier(DRAWER_SETTINGS_ID).withName("Settings")
-			)
-			.withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
-					FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
-					switch(drawerItem.getIdentifier()) {
-						case DRAWER_MUSIC_ID: {
-							Bundle data = new Bundle(1);
-							data.putString(Const.KEY_ROOT, rootMusicDir.getPath());
-							final LibraryFragment fragment = new LibraryFragment();
-							fragment.setOnFileSelectionListener(new LibraryFragment.OnFileSelectionListener() {
-								@Override
-								public void onFileSelection(File file) {
-									playFile(file);
-								}
-							});
-							fragment.setArguments(data);
-							fragTrans.replace(R.id.fragment_container, fragment, DRAWER_MUSIC_TAG);
-							fragTrans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-							fragTrans.commit();
-							break;
-						}
-						case DRAWER_SETTINGS_ID: {
-							final EqualizerFragment fragment = new EqualizerFragment();
-							fragTrans.replace(R.id.fragment_container, fragment, DRAWER_SETTINGS_TAG);
-							fragTrans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-							fragTrans.commit();
-							break;
+				.withActivity(this)
+				.withToolbar(toolbar)
+				.withDrawerWidthDp(255)
+				.withDrawerGravity(Gravity.LEFT)
+				.addDrawerItems(
+						new PrimaryDrawerItem().withIdentifier(DRAWER_MUSIC_ID).withName("Home"),
+						new DividerDrawerItem(),
+						new SecondaryDrawerItem().withIdentifier(DRAWER_SETTINGS_ID).withName("Settings")
+				)
+				.withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+						FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
+						switch (drawerItem.getIdentifier()) {
+							case DRAWER_MUSIC_ID: {
+								switchToLibraryFragment(fragTrans);
+								break;
+							}
+							case DRAWER_SETTINGS_ID: {
+								switchToEqualizerFragment(fragTrans);
+								break;
+							}
 						}
 					}
-				}
-			})
-			.build();
+				})
+				.build();
 
 		rightDrawer = new Drawer()
-			.withActivity(this)
-			.withDrawerGravity(Gravity.RIGHT)
-			.addDrawerItems(
-				new PrimaryDrawerItem().withName("Volume"),
-				new DividerDrawerItem(),
-				new SecondaryDrawerItem().withName("Misc")
-			)
-			.withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
-				}
-			})
-			.append(leftDrawer);
+				.withActivity(this)
+				.withDrawerGravity(Gravity.RIGHT)
+				.addDrawerItems(
+						new PrimaryDrawerItem().withName("Volume"),
+						new DividerDrawerItem(),
+						new SecondaryDrawerItem().withName("Misc")
+				)
+				.withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+					}
+				})
+				.append(leftDrawer);
 	}
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if(keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
-			if(leftDrawer != null && leftDrawer.isDrawerOpen()) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+			if (leftDrawer != null && leftDrawer.isDrawerOpen()) {
 				leftDrawer.closeDrawer();
 				return true;
 			} else if (rightDrawer != null && rightDrawer.isDrawerOpen()) {
@@ -186,7 +158,7 @@ public class MainActivity extends ActionBarActivity {
 				return true;
 			}
 		}
-		BaseFragment f = (BaseFragment)getSupportFragmentManager().findFragmentByTag(DRAWER_MUSIC_TAG);
+		BaseFragment f = (BaseFragment) getSupportFragmentManager().findFragmentByTag(DRAWER_MUSIC_TAG);
 		return f != null && f.onKeyDown(keyCode, event);
 	}
 
@@ -224,7 +196,30 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void debug(String s, Object ... args) {
+	public void switchToLibraryFragment(FragmentTransaction fragTrans) {
+		Bundle data = new Bundle(1);
+		data.putString(Const.KEY_ROOT, rootMusicDir.getPath());
+		final LibraryFragment fragment = new LibraryFragment();
+		fragment.setOnFileSelectionListener(new LibraryFragment.OnFileSelectionListener() {
+			@Override
+			public void onFileSelection(File file) {
+				playFile(file);
+			}
+		});
+		fragment.setArguments(data);
+		fragTrans.replace(R.id.fragment_container, fragment, DRAWER_MUSIC_TAG)
+				.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+				.commit();
+	}
+
+	public void switchToEqualizerFragment(FragmentTransaction fragTrans) {
+		final EqualizerFragment fragment = new EqualizerFragment();
+		fragTrans.replace(R.id.fragment_container, fragment, DRAWER_SETTINGS_TAG)
+				.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+				.commit();
+	}
+
+	private void debug(String s, Object... args) {
 		Log.d(this.getClass().getSimpleName(), String.format(s, args));
 	}
 }
